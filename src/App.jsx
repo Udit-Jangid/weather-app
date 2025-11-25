69db2195f3c3d948cef086373e03cbb45bbd9870
Udit-Jangid <uditjangid20012015@gmail.com>
2025-11-25 19:10:45 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Temp from "./components/weather/temp";
+
+function App() {
+  // 82839af0afc76fcde48b28e2db1ba374
+  // https://api.openweathermap.org/data/2.5/weather?q=pune&appid=82839af0afc76fcde48b28e2db1ba374
+  return (
+    <>
+      <Temp />
+    </>
+  );
+}
+
+export default App;
